05697ca89a4f6abb08dfba4cc7936e79139c84bd
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-03-11 12:01:39 +0300

    Initial Commit

---
+++ Multi-Screen Task Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Multi-Screen Task Manager
+//
+//  Created by عبدالله حافظ on 11/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Multi-Screen Task Manager/Multi_Screen_Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_Screen_Task_ManagerApp.swift
+//  Multi-Screen Task Manager
+//
+//  Created by عبدالله حافظ on 11/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Multi_Screen_Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
